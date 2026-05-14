269afb9b1647e06c19130616e2a4af30d2f65a1c
shatadru06 <shatadru.mandal@cloudkaptan.com>
2026-05-14 23:55:14 +0530

    khk

--- scripts/validateComponents.sh
+++ scripts/validateComponents.sh
@@ -13,7 +13,7 @@ function validateComponent() {
   then
     if grep -q '<name>ApexClass</name>' $PACKAGE_XML; then
           # Initiating async deployment with RunSpecifiedTests.
-          sf project deploy start -o $TARGET_ORG_ALIAS -x $PACKAGE_XML -l RunSpecifiedTests -tests "$(cat manifest/testclass/testclass.txt)" --verbose --dry-run --async --ignore-conflicts | tee $RESULT_FILE 
+          sf project deploy start -o $TARGET_ORG_ALIAS -x $PACKAGE_XML -l RunSpecifiedTests --tests "$(cat manifest/testclass/testclass.txt)" --verbose --dry-run --async --ignore-conflicts | tee $RESULT_FILE 
       else
           # Initiating async deployment without specifying test classes.
           sf project deploy start -o $TARGET_ORG_ALIAS -x $PACKAGE_XML --verbose --dry-run --async --ignore-conflicts | tee $RESULT_FILE
