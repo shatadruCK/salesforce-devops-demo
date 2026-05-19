4a69a0ae2c8cc74cbd5b7f271a700199a8cf72cc
shatadru06 <shatadru.mandal@cloudkaptan.com>
2026-05-19 10:42:22 +0530

    ohlhl

--- cicd-utils/scripts-util/validateComponents.sh
+++ cicd-utils/scripts-util/validateComponents.sh
@@ -20,7 +20,6 @@ performDeltaCheckOnlyDeployment() {
         # Initiating async deployment.
         if grep -q '<name>ApexClass</name>' $packageXmlFilePath; then
             # Initiating async deployment with RunSpecifiedTests.
-            $(cat $runTestClassesFilePath)
             sf project deploy start -o $AUTH_ORG_ALIAS -x $packageXmlFilePath -l RunSpecifiedTests --tests HelloWorldTest --verbose --dry-run --async | tee $deployOrgFilePath
         else
             # Initiating async deployment without specifying test classes.
